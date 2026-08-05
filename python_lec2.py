274d38fac05a8d68df4bb944c2beb3c19b6d93ef
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-05 12:52:44 +0100

    lec 2 conditions

--- python_lec2.py
+++ python_lec2.py
@@ -12,13 +12,30 @@
 
 
 # WAP if a number entered by user is even or odd 
-num = int(input("ENter the number please:"))
+# num = int(input("ENter the number please:"))
 
-if(num% 2 == 0):
-    print("num is even ")
-elif(num% 3 == 0):
-    print("num is odd")
+# if(num% 2 == 0):
+#     print("num is even ")
+# elif(num% 3 == 0):
+#     print("num is odd")
+# else:
+#     print("num is neither even nor odd")
+
+# print("end of the program")
+
+
+# WAP to enter three numbers and find the greatest among them
+print("You need to enter three values and we check which is greater among them")
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number:"))
+
+print('You entered:',a ,b ,c)
+
+if(a>b and a>c):
+    print(a, 'is greater')
+elif(b>a and b>c):
+    print(b, 'is greater')
 else:
-    print("num is neither even nor odd")
-
-print("end of the program")
+    print(c, 'is greater')
