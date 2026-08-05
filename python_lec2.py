5cac71d2fe5fe4ecb2feb07845a988bc9e02a4d2
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-05 12:47:22 +0100

    lec 2 conditions

--- python_lec2.py
+++ python_lec2.py
@@ -1,11 +1,24 @@
-name = input("What is your name? ")
-strLen= len(name)
+# # This is a simple python program to demonstrate the use of string functions
+# name = input("What is your name? ")
+# strLen= len(name)
 
-print('Name you entered is:', name)
-print('Length of the name is:', strLen)
+# print('Name you entered is:', name)
+# print('Length of the name is:', strLen)
 
-str = "Hi I am $ and my simbol is $ and worth is $99.99"
-occurences = str.count('$')
-print('Number of occurences of letter $ in the string is:', occurences)
+# #  find the number of occurences of a letter in a string
+# str = "Hi I am $ and my simbol is $ and worth is $99.99"
+# occurences = str.count('$')
+# print('Number of occurences of letter $ in the string is:', occurences)
 
 
+# WAP if a number entered by user is even or odd 
+num = int(input("ENter the number please:"))
+
+if(num% 2 == 0):
+    print("num is even ")
+elif(num% 3 == 0):
+    print("num is odd")
+else:
+    print("num is neither even nor odd")
+
+print("end of the program")
