709dc490bcdcba07bd29d5f70863e84d25397a18
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-05 02:37:44 +0100

    lec1

---
+++ python_lec1.py
@@ -0,0 +1,16 @@
+print("Hello World!", 'Its me Abdullah Mir')
+
+name = 'Mir'
+age = 25
+hight= 5.9
+
+print('My name is', name, 'and I am', age, 'years old. My height is', hight, 'feet.')
+
+print(type(name))
+
+# enter two nums and print their sum
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+sum = num1 + num2
+print('The sum of', num1, 'and', num2, 'is', sum)
