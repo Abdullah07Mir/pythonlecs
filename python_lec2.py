ac9a7627cb0c2f3203cf8712da68f93de8662f23
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-05 12:13:15 +0100

    lec 2 strings

---
+++ python_lec2.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+strLen= len(name)
+
+print('Name you entered is:', name)
+print('Length of the name is:', strLen)
+
+str = "Hi I am $ and my simbol is $ and worth is $99.99"
+occurences = str.count('$')
+print('Number of occurences of letter $ in the string is:', occurences)
+
+
