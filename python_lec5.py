7ff010a710975c6ed7eea0d3d7f5f33c9c5e7a98
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-06 13:33:28 +0100

    lec 5 loops

--- python_lec5.py
+++ python_lec5.py
@@ -1,13 +1,21 @@
-num = 1
-while num<=100:
-    print(num)
-    num+=1
+# num = 1
+# while num<=100:
+#     print(num)
+#     num+=1
 
-print("end of the program")
+# print("end of the program")
 
-num = 100
-while num>=1:
-    print(num)
-    num-=1
+# num = 100
+# while num>=1:
+#     print(num)
+#     num-=1
 
+# print("end of the program")
+
+val = int(input("Enter a number you want the multiplication table for:"))
+i=1
+while i<=10:
+    z=val*i
+    print(z)
+    i+=1
 print("end of the program")
