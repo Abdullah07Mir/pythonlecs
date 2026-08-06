1a2f67ee81afe0592c3b693e0b7980b2b4f4b986
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-06 13:05:31 +0100

    lec 4 dictionary

--- python_lec4.py
+++ python_lec4.py
@@ -1,8 +1,13 @@
-dict = {}
-dict.update({
-    "table":("a piece of furniture", "list of facts and figures"),
-    "cat": "small animal"
-})
+# dict = {}
+# dict.update({
+#     "table":("a piece of furniture", "list of facts and figures"),
+#     "cat": "small animal"
+# })
 
-print(dict)
-print(type(dict))
+# print(dict)
+# print(type(dict))
+
+set = {"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"}
+print(set)
+print(len(set))
+
