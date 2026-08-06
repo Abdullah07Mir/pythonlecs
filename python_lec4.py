d584053201b8d906a0a0408a454b3af5600b1354
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-06 13:09:52 +0100

    lec 4 sets

--- python_lec4.py
+++ python_lec4.py
@@ -1,3 +1,5 @@
+#  WAP to enter three favourite movie names and store them in a list and print the list
+
 # dict = {}
 # dict.update({
 #     "table":("a piece of furniture", "list of facts and figures"),
@@ -7,7 +9,19 @@
 # print(dict)
 # print(type(dict))
 
-set = {"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"}
-print(set)
-print(len(set))
+# You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
+# set = {"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"}
+# print(set)
+# print(len(set))
 
+
+sub_marks={}
+chem = int(input("Enter marks of chemistry:"))
+phy = int(input("Enter marks of physics:"))
+maths = int(input("Enter marks of maths:"))
+sub_marks.update({
+    "chemistry": chem,
+    "physics": phy,
+    "maths": maths
+})
+print(sub_marks)
