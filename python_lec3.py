567c8397f64bdfd9f45626317e4e179e45f7e2c5
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-06 03:13:22 +0100

    lec 3 lists

---
+++ python_lec3.py
@@ -0,0 +1,10 @@
+#  WAP to enter three favourite movie names and store them in a list and print the list 
+movie_list = []
+movie1 = input("Enter your first fav movie name:")
+movie2 = input("Enter your second fav movie name:")
+movie3 = input("Enter your third fav movie name:")
+
+movie_list.append(movie1)
+movie_list.append(movie2)
+movie_list.append(movie3)
+print("Your favourite movies are:", movie_list)
