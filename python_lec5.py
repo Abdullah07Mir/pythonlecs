cc523824e636e3a2d2c9cab080e1ac37ceaeee7e
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-07 03:25:45 +0100

    lec 5 loops

--- python_lec5.py
+++ python_lec5.py
@@ -1,3 +1,5 @@
+#  WAP to print the numbers from 1 to 100 using while loop.
+
 # num = 1
 # while num<=100:
 #     print(num)
@@ -12,6 +14,8 @@
 
 # print("end of the program")
 
+# WAP to print the multiplication table of a number using while loop.
+
 # val = int(input("Enter a number you want the multiplication table for:"))
 # i=1
 # while i<=10:
@@ -19,6 +23,8 @@
 #     print(z)
 #     i+=1
 # print("end of the program")
+
+# WAP to print the elements of a list using while loop.
 
 # x =[1,4,9,16,25,36,49,64,81,100]
 # i=0
@@ -44,8 +50,23 @@
 
 # print("end of the program ")
 
-el_list =[1,4,9,16,25,36,49,64,81,100]
+# WAP to print the elements of a list using for loop.
 
-print("ELements of the list are:")
-for i in el_list:
-    print(i)
+# el_list =[1,4,9,16,25,36,49,64,81,100]
+
+# print("ELements of the list are:")
+# for i in el_list:
+#     print(i)
+
+# WAP to search a value in a tuple using for loop and if else statement.
+
+el_tup = (1,4,9,16,25,36,49,64,81,100)
+
+find = int(input("Enter the value you want to find:"))
+
+for x in el_tup:
+    if(x == find):
+        print('value found in tuple')
+        break
+else:
+    print('value not found')
