c488405793e790f223cfef0a27000abee1aa6ea3
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-07 12:19:41 +0100

    lec 5 loops

--- python_lec5.py
+++ python_lec5.py
@@ -60,13 +60,16 @@
 
 # WAP to search a value in a tuple using for loop and if else statement.
 
-el_tup = (1,4,9,16,25,36,49,64,81,100)
+# el_tup = (1,4,9,16,25,36,49,64,81,100)
 
-find = int(input("Enter the value you want to find:"))
+# find = int(input("Enter the value you want to find:"))
 
-for x in el_tup:
-    if(x == find):
-        print('value found in tuple')
-        break
-else:
-    print('value not found')
+# for x in el_tup:
+#     if(x == find):
+#         print('value found in tuple')
+#         break
+# else:
+#     print('value not found')
+
+for y in range(101):
+    print(y) 
