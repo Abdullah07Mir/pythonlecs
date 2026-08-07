2606f2cbfa792677617b9e7e39bde3f4ed32084f
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-07 03:16:17 +0100

    lec 5 loops

--- python_lec5.py
+++ python_lec5.py
@@ -27,18 +27,25 @@
 #     i+=1
 # print("end of the program")
 
-tup = (1,4,9,16,25,36,49,64,81,100)
+# WAP to search a value in a tuple using while loop and if else statement.  
+# tup = (1,4,9,16,25,36,49,64,81,100)
 
-ser= int(input("Enter value to search:"))
+# ser= int(input("Enter value to search:"))
 
-i=0
+# i=0
 
-while i< len(tup):
-    if(tup[i]==ser):
-        print("value found at index:", i)
-        break
-    i+=1
-else:
-    print("value not found in the tuple")
+# while i< len(tup):
+#     if(tup[i]==ser):
+#         print("value found at index:", i)
+#         break
+#     i+=1
+# else:
+#     print("value not found in the tuple")
 
-print("end of the program ")
+# print("end of the program ")
+
+el_list =[1,4,9,16,25,36,49,64,81,100]
+
+print("ELements of the list are:")
+for i in el_list:
+    print(i)
