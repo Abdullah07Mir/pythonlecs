1c79cd5622a461a6284168673f133574e39b04a1
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-07 12:43:31 +0100

    lec 5 loops

--- python_lec5.py
+++ python_lec5.py
@@ -1,85 +1,86 @@
 #  WAP to print the numbers from 1 to 100 using while loop.
 
-# num = 1
-# while num<=100:
-#     print(num)
-#     num+=1
+num = 1
+while num<=100:
+    print(num)
+    num+=1
 
-# print("end of the program")
+print("end of the program")
 
-# num = 100
-# while num>=1:
-#     print(num)
-#     num-=1
+num = 100
+while num>=1:
+    print(num)
+    num-=1
 
-# print("end of the program")
+print("end of the program")
 
 # WAP to print the multiplication table of a number using while loop.
 
-# val = int(input("Enter a number you want the multiplication table for:"))
-# i=1
-# while i<=10:
-#     z=val*i
-#     print(z)
-#     i+=1
-# print("end of the program")
+val = int(input("Enter a number you want the multiplication table for:"))
+i=1
+while i<=10:
+    z=val*i
+    print(z)
+    i+=1
+print("end of the program")
 
 # WAP to print the elements of a list using while loop.
 
-# x =[1,4,9,16,25,36,49,64,81,100]
-# i=0
-# while i<= len(x)-1:
-#     print("the list element no. i is:", x[i])
-#     i+=1
-# print("end of the program")
+x =[1,4,9,16,25,36,49,64,81,100]
+i=0
+while i<= len(x)-1:
+    print("the list element no. i is:", x[i])
+    i+=1
+print("end of the program")
 
 # WAP to search a value in a tuple using while loop and if else statement.  
-# tup = (1,4,9,16,25,36,49,64,81,100)
+tup = (1,4,9,16,25,36,49,64,81,100)
 
-# ser= int(input("Enter value to search:"))
+ser= int(input("Enter value to search:"))
 
-# i=0
+i=0
 
-# while i< len(tup):
-#     if(tup[i]==ser):
-#         print("value found at index:", i)
-#         break
-#     i+=1
-# else:
-#     print("value not found in the tuple")
+while i< len(tup):
+    if(tup[i]==ser):
+        print("value found at index:", i)
+        break
+    i+=1
+else:
+    print("value not found in the tuple")
 
-# print("end of the program ")
+print("end of the program ")
 
 # WAP to print the elements of a list using for loop.
 
-# el_list =[1,4,9,16,25,36,49,64,81,100]
+el_list =[1,4,9,16,25,36,49,64,81,100]
 
-# print("ELements of the list are:")
-# for i in el_list:
-#     print(i)
+print("ELements of the list are:")
+for i in el_list:
+    print(i)
 
 # WAP to search a value in a tuple using for loop and if else statement.
 
-# el_tup = (1,4,9,16,25,36,49,64,81,100)
+el_tup = (1,4,9,16,25,36,49,64,81,100)
 
-# find = int(input("Enter the value you want to find:"))
+find = int(input("Enter the value you want to find:"))
 
-# for x in el_tup:
-#     if(x == find):
-#         print('value found in tuple')
-#         break
-# else:
-#     print('value not found')
+for x in el_tup:
+    if(x == find):
+        print('value found in tuple')
+        break
+else:
+    print('value not found')
 
 # WAP to print the numbers from 1 to 100 using for loop.
-# for y in range(1, 101):
-#     print(y) 
+for y in range(1, 101):
+    print(y) 
 
 # WAP to print the numbers from 100 to 1 using for loop.
-# for z in range(100, 0, -1):
-#     print(z) 
+for z in range(100, 0, -1):
+    print(z) 
 
 # WAP to print the multiplication table of a number using for loop.
 mul = int(input("Enter a value you want to write the table for:"))
 for c in range(1,11):
     print(mul*c)
+
---
+++ python_lec5_prac_Qs.py
@@ -0,0 +1,10 @@
+# WAP to find the sum of n numbers using while loop.
+n = int(input("Enter total values you want sum for:"))
+sum =0
+i=1
+while i<n:
+    sum = sum+i
+    i+=1
+print("SUm after adding i in existing total is :", sum)
+
+print("end of the program")
