896518106ff6fc3643f968479d7faee01ab95f84
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-08 01:50:39 +0100

    lec 5 loops

--- python_lec5_prac_Qs.py
+++ python_lec5_prac_Qs.py
@@ -1,13 +1,13 @@
 # WAP to find the sum of n numbers using while loop.
-# n = int(input("Enter total values you want sum for:"))
-# sum =0
-# i=1
-# while i<n:
-#     sum = sum+i
-#     i+=1
-# print("Sum after adding i in existing total is :", sum)
+n = int(input("Enter total values you want sum for:"))
+sum =0
+i=1
+while i<n:
+    sum = sum+i
+    i+=1
+print("Sum after adding i in existing total is :", sum)
 
-# print("end of the program")
+print("end of the program")
 
 # WAP to find the factorial of n numbers.
 x = int(input("Enter total values you want factorial for:"))
