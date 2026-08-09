b9a87f012b5c139903cc7dbcad300acdaabfdc52
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-09 21:42:32 +0100

    lec 7 functions

--- python_lec7.py
+++ python_lec7.py
@@ -1,49 +1,53 @@
-# f = open("practice.txt", "w")
-# f.write("Hi everyone \n")
-# f.close()
+#  Create a new file “practice.txt” using python. Add data in it:
 
-# x = open("practice.txt", "a+")
-# x.write("We are learning File I/O \n")
-# x.write("using Java. \n")
-# x.write("I like programming in Java. \n")
-# x.seek(0)
-# data= x.read()
-# print("...", data)
-# x.close()
+f = open("practice.txt", "w")
+f.write("Hi everyone \n")
+f.close()
 
-# with open("practice.txt", "r+") as y:
-#     data_in_file = y.read()
-#     print(data_in_file)
-#     if("Java" in data_in_file):
-#         newdata_in_file= data_in_file.replace("Java","Python")
-#     y.seek(0)
-#     y.write(newdata_in_file)
-#     y.close()
+x = open("practice.txt", "a+")
+x.write("We are learning File I/O \n")
+x.write("using Java. \n")
+x.write("I like programming in Java. \n")
+x.seek(0)
+data= x.read()
+print("...", data)
+x.close()
 
+# WAF that replace all occurrences of “java” with “python” in above file.
+with open("practice.txt", "r+") as y:
+    data_in_file = y.read()
+    print(data_in_file)
+    if("Java" in data_in_file):
+        newdata_in_file= data_in_file.replace("Java","Python")
+    y.seek(0)
+    y.write(newdata_in_file)
+    y.close()
 
-# with open("practice.txt", "r") as z:
-#     search_data = z.read()
-#     if("learning" in search_data):
-#         print("Found")
-#     else:
-#         print("Not found")
-# z.close()
+# Search if the word “learning” exists in the file or not.
+with open("practice.txt", "r") as z:
+    search_data = z.read()
+    if("learning" in search_data):
+        print("Found")
+    else:
+        print("Not found")
+z.close()
 
-# with open("practice.txt", "r") as a:
-#     i = 0
-#     line = a.readline()
-#     while  line!= "":
-#         if("learning" in a.readline()):
-#             print("Found learning in line number :", i)
-#             break
-#         else:
-#             line = a.readline()
-#             i+=1
-#     print("Not Found at any line")
+# WAF to find in which line of the file does the word “learning”occur first. Print -1 if word not found.
+with open("practice.txt", "r") as a:
+    i = 0
+    line = a.readline()
+    while  line!= "":
+        if("learning" in a.readline()):
+            print("Found learning in line number :", i)
+            break
+        else:
+            line = a.readline()
+            i+=1
+    print("Not Found at any line")
 
-# a.close()
+a.close()
 
-
+# From a file containing numbers separated by comma, print the count of even numbers.
 with open("countFile.txt", "w")as c:
     c.write("1,2,5,4,6,3,7,8,9,12,10")
     c.close()
