765386577bddf8297a146cee53b2546feae4ca2f
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-09 21:40:10 +0100

    lec 7 functions

--- python_lec7.py
+++ python_lec7.py
@@ -1,47 +1,47 @@
-f = open("practice.txt", "w")
-f.write("Hi everyone \n")
-f.close()
+# f = open("practice.txt", "w")
+# f.write("Hi everyone \n")
+# f.close()
 
-x = open("practice.txt", "a+")
-x.write("We are learning File I/O \n")
-x.write("using Java. \n")
-x.write("I like programming in Java. \n")
-x.seek(0)
-data= x.read()
-print("...", data)
-x.close()
+# x = open("practice.txt", "a+")
+# x.write("We are learning File I/O \n")
+# x.write("using Java. \n")
+# x.write("I like programming in Java. \n")
+# x.seek(0)
+# data= x.read()
+# print("...", data)
+# x.close()
 
-with open("practice.txt", "r+") as y:
-    data_in_file = y.read()
-    print(data_in_file)
-    if("Java" in data_in_file):
-        newdata_in_file= data_in_file.replace("Java","Python")
-    y.seek(0)
-    y.write(newdata_in_file)
-    y.close()
+# with open("practice.txt", "r+") as y:
+#     data_in_file = y.read()
+#     print(data_in_file)
+#     if("Java" in data_in_file):
+#         newdata_in_file= data_in_file.replace("Java","Python")
+#     y.seek(0)
+#     y.write(newdata_in_file)
+#     y.close()
 
 
-with open("practice.txt", "r") as z:
-    search_data = z.read()
-    if("learning" in search_data):
-        print("Found")
-    else:
-        print("Not found")
-z.close()
+# with open("practice.txt", "r") as z:
+#     search_data = z.read()
+#     if("learning" in search_data):
+#         print("Found")
+#     else:
+#         print("Not found")
+# z.close()
 
-with open("practice.txt", "r") as a:
-    i = 0
-    line = a.readline()
-    while  line!= "":
-        if("learning" in a.readline()):
-            print("Found learning in line number :", i)
-            break
-        else:
-            line = a.readline()
-            i+=1
-    print("Not Found at any line")
+# with open("practice.txt", "r") as a:
+#     i = 0
+#     line = a.readline()
+#     while  line!= "":
+#         if("learning" in a.readline()):
+#             print("Found learning in line number :", i)
+#             break
+#         else:
+#             line = a.readline()
+#             i+=1
+#     print("Not Found at any line")
 
-a.close()
+# a.close()
 
 
 with open("countFile.txt", "w")as c:
@@ -50,13 +50,16 @@ with open("countFile.txt", "w")as c:
 
 with open("countFile.txt", "r") as num:
     count_even = num.read()
-    newcount_even = int(count_even)
+    print("the data in file is", count_even)
+    newcount_even  = list(count_even.split(','))
+    new_list = [int(x) for x in newcount_even]
     q = 0
-    for v in newcount_even:
+    for v in new_list:
         if(v % 2 == 0):
             q+=1
     print("Count of even numbers in file are:", q )
     num.close()        
+
 
 
      
