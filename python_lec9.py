ac71b4bce81a75063f378f958f7de65ebaf2dbec
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-12 15:32:59 +0100

    all 9 lec code of python

--- python_lec9.py
+++ python_lec9.py
@@ -1,43 +1,44 @@
-# WAP 
-# class Circle:
-#     def __init__(self, radius):
-#         self.radius =  radius
+# WAP to calculate radius and parameters of circle
+class Circle:
+    def __init__(self, radius):
+        self.radius =  radius
 
-#     def area(self):
-#         self.area = 3.14* self.radius**2
-#         return self.area
-#     def  parameter(self):
-#         self.parameter = 2*3.14*self.radius
-#         return self.parameter
-# c1 = Circle(21)
-# print("The area of the circle is :", c1.area())
-# print("The parameter of the circle is :", c1.parameter())
+    def area(self):
+        self.area = 3.14* self.radius**2
+        return self.area
+    def  parameter(self):
+        self.parameter = 2*3.14*self.radius
+        return self.parameter
+c1 = Circle(21)
+print("The area of the circle is :", c1.area())
+print("The parameter of the circle is :", c1.parameter())
 
-# class Employee:
-#     def __init__(self, role, department, salary):
-#         self.role = role
-#         self.department = department
-#         self.salary =  salary
+# Employee details inheritance code
+class Employee:
+    def __init__(self, role, department, salary):
+        self.role = role
+        self.department = department
+        self.salary =  salary
 
-#     def show_details(self):
+    def show_details(self):
         
-#         print("Role: ", self.role, "\n", "Department: ", self.department, "\n", "Salary: ", self.salary )
+        print("Role: ", self.role, "\n", "Department: ", self.department, "\n", "Salary: ", self.salary )
 
-# class Engineer(Employee):
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#         super().__init__("Data Scientist", "Tech", 60000)
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("Data Scientist", "Tech", 60000)
 
-#     def output(self):
-#         print("__Employee details__")
-#         print("Name: ",self.name, "\n", "Age: ", self.age)
-#         super().show_details()
+    def output(self):
+        print("__Employee details__")
+        print("Name: ",self.name, "\n", "Age: ", self.age)
+        super().show_details()
 
-# e1 = Engineer("Abdullah Mir", 25)
-# e1.output()
+e1 = Engineer("Abdullah Mir", 25)
+e1.output()
         
-
+# polymorphism to check which order is greater
 class Orders:
     def __init__(self, item, price):
         self.item = item
