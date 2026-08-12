19278977cd8d02a414b461bf99d2a272694a0773
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-12 15:15:46 +0100

    all 9 lec code of python

--- python_lec9.py
+++ python_lec9.py
@@ -13,27 +13,45 @@
 # print("The area of the circle is :", c1.area())
 # print("The parameter of the circle is :", c1.parameter())
 
-class Employee:
-    def __init__(self, role, department, salary):
-        self.role = role
-        self.department = department
-        self.salary =  salary
+# class Employee:
+#     def __init__(self, role, department, salary):
+#         self.role = role
+#         self.department = department
+#         self.salary =  salary
 
-    def show_details(self):
+#     def show_details(self):
         
-        print("Role: ", self.role, "\n", "Department: ", self.department, "\n", "Salary: ", self.salary )
+#         print("Role: ", self.role, "\n", "Department: ", self.department, "\n", "Salary: ", self.salary )
 
-class Engineer(Employee):
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        super().__init__("Data Scientist", "Tech", 60000)
+# class Engineer(Employee):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         super().__init__("Data Scientist", "Tech", 60000)
 
-    def output(self):
-        print("__Employee details__")
-        print("Name: ",self.name, "\n", "Age: ", self.age)
-        super().show_details()
+#     def output(self):
+#         print("__Employee details__")
+#         print("Name: ",self.name, "\n", "Age: ", self.age)
+#         super().show_details()
 
-e1 = Engineer("Abdullah Mir", 25)
-e1.output()
+# e1 = Engineer("Abdullah Mir", 25)
+# e1.output()
         
+
+class Orders:
+    def __init__(self, item, price):
+        self.item = item
+        self.price = price
+    def show(self):
+        print("The item is :",self.item, "and its price is:", self.price)
+
+    def __gt__(self, ord2):
+        if(self.price > ord2.price):
+            print("Order 1 is greater than Order 2")
+        else:
+            print("Order 2 is greater than Order 1")
+
+o1 = Orders("Milk", 2)
+o2 = Orders("Toy", 15)
+
+o1 > o2
