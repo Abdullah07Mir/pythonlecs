976d1c714980aa38d0813c2d92de902ee64389bd
Abdullah Mir <98210639+Abdullah07Mir@users.noreply.github.com>
2026-08-12 14:40:59 +0100

    lec 9 classes

--- python_lec9.py
+++ python_lec9.py
@@ -1,13 +1,28 @@
-class Circle:
-    def __init__(self, radius):
-        self.radius =  radius
+# WAP 
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius =  radius
 
-    def area(self):
-        self.area = 3.14* self.radius**2
-        return self.area
-    def  parameter(self):
-        self.parameter = 2*3.14*self.radius
-        return self.parameter
-c1 = Circle(21)
-print("The area of the circle is :", c1.area())
-print("The parameter of the circle is :", c1.parameter())
+#     def area(self):
+#         self.area = 3.14* self.radius**2
+#         return self.area
+#     def  parameter(self):
+#         self.parameter = 2*3.14*self.radius
+#         return self.parameter
+# c1 = Circle(21)
+# print("The area of the circle is :", c1.area())
+# print("The parameter of the circle is :", c1.parameter())
+
+class Employee:
+    def __init__(self, role, department, salary):
+        self.role = role
+        self.department = department
+        self.salary =  salary
+
+    def show_details(self):
+        print("__Employee details__")
+        print("Role: ", self.role, "\n", "Department:", self.department, "\n", "Salary:", self.salary )
+
+e1 = Employee("Data Scientist", "Tech", 60000)
+e1.show_details()
+        
